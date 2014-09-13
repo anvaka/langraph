b1eead7a01d2b701542c2efdffed3dee0bdef107
anvaka <anvaka@gmail.com>
2014-09-13 13:52:11 -0700

    Extracted graph creation into separate file

--- src/scripts/appController.js
+++ src/scripts/appController.js
@@ -1,65 +1,28 @@
-var templateParser = require('./templateParser');
-
 require('an').controller(AppController);
 
 function AppController($scope, $http, $q) {
   var wiki = require('./wikipediaClient')($http, $q);
-  var foundLanguages = {};
-
   $scope.logMessage = 'Building wikipedia graph...';
 
-  // todo: this should really be a graph builder, not app controller:
-  wiki.getAllLanguages()
-    .then(getPageContent)
-    .then(parseInfoBox)
-    .then(addCrossLinks)
-    .then(log);
-
-  function getPageContent(languages) {
+  var graphBuilder = require('./languageInfluenceGraphBuilder')(wiki);
+  var graph = {}; // <--- should be instance of ngraph
+  graphBuilder.build(graph).then(function(result) {
     $scope.logMessage = 'Done. Getting page content';
-    $scope.languages = languages.map(toView);
-    $scope.languages.forEach(save);
+    $scope.languages = Object.keys(result).map(toValue).map(toView);
 
-    return wiki.getPages(languages.map(toPageid));
-
-    function save(language) {
-      if (!language.title) debugger;
-      foundLanguages[language.title] = language;
+    function toValue(key) {
+      return result[key];
     }
-  }
+  }).then(log);
 
-  function parseInfoBox(pages) {
-    pages.map(toInfoBox).forEach(saveInfobox);
-
-    return foundLanguages;
-
-    function saveInfobox(infoBox) {
-      if (!infoBox.title) debugger;
-      foundLanguages[infoBox.title].info = infoBox.info;
-    }
-  }
-
-  function addCrossLinks(languages) {
-    Object.keys(languages).forEach(addLinks);
-    return foundLanguages;
-
-    function addLinks(key) {
-      var info = languages[key].info;
-      info.parsedInfluenced.forEach(checkLinkExist);
-      info.parsedInfluencedBy.forEach(checkLinkExist);
-    }
-
-    function checkLinkExist(language) {
-      language.ref = foundLanguages[language.link];
-    }
-  }
 }
 
 function toView(language) {
   return {
     pageid: language.pageid,
     url: '//www.wikipedia.org/wiki/' + escapeWikiUrl(language.title),
-    title: language.title
+    title: language.title,
+    info: language.info
   };
 }
 
@@ -70,62 +33,5 @@ function log(message) {
 function escapeWikiUrl(name) {
   return name.replace(/\s/g, '_');
 }
-
-function toPageid(language) {
-  return language.pageid;
-}
-
-
-function toInfoBox(page) {
-  console.log('processing', page.title);
-  var info = templateParser(page.revisions[0]['*']);
-  info.parsedYear = sanitizeDates(info);
-  info.parsedInfluenced = parseLanguagesList(info.influenced);
-  info.parsedInfluencedBy = parseLanguagesList(info.influenced_by || info['influenced by']);
-
-  return {
-    title: page.title,
-    id: page.pageid,
-    info: info
-  };
-}
-
-function parseLanguagesList(wikiText) {
-  var result = [];
-  var knownLanguages = Object.create(null);
-  if (!wikiText) return result;
-
-  var languageRegex = /\[\[(.+?)(?:\|(.+?))?\]\]/g;
-  var match;
-
-  while ((match = languageRegex.exec(wikiText))) {
-    var languageLink = match[1];
-    if (knownLanguages[languageLink]) continue;
-    knownLanguages[languageLink] = 1;
-
-    result.push({
-      name: match[2] || languageLink,
-      link: languageLink
-    });
-  }
-
-  return result;
-}
-
-function sanitizeDates(infoBox) {
-  return probe(infoBox.year) ||
-    probe(infoBox.released) ||
-    probe(infoBox.latest_release_date) ||
-    probe(infoBox['latest release date']);
-
-  function probe(year) {
-    if (!year) return;
-    var numericMatch = year.match(/((?:19\d\d)|(?:2\d{3}))/);
-    if (numericMatch) {
-      return parseInt(numericMatch[1], 10);
-    }
-  }
-}
-
 
 AppController.$inject = ['$scope', '$http', '$q'];
---
+++ src/scripts/languageInfluenceGraphBuilder.js
@@ -0,0 +1,106 @@
+var templateParser = require('./templateParser');
+
+module.exports = languageInfluenceGraphBuilder;
+
+function languageInfluenceGraphBuilder(wikiClient) {
+  var foundLanguages = {};
+
+  return {
+    build: build
+  };
+
+  function build(graph) {
+    return wikiClient.getAllLanguages()
+      .then(getPageContent)
+      .then(parseInfoBox)
+      .then(addCrossLinks);
+  }
+
+  function getPageContent(languages) {
+    languages.forEach(save);
+    return wikiClient.getPages(languages.map(toPageid));
+  }
+
+  function parseInfoBox(pages) {
+    pages.map(toInfoBox).forEach(saveInfobox);
+    return foundLanguages;
+  }
+
+  function toInfoBox(page) {
+    console.log('processing', page.title);
+    var info = templateParser(page.revisions[0]['*']);
+    info.parsedYear = sanitizeDates(info);
+    info.parsedInfluenced = parseLanguagesList(info.influenced);
+    info.parsedInfluencedBy = parseLanguagesList(info.influenced_by || info['influenced by']);
+
+    return {
+      title: page.title,
+      id: page.pageid,
+      info: info
+    };
+  }
+
+  function parseLanguagesList(wikiText) {
+    var result = [];
+    var knownLanguages = Object.create(null);
+    if (!wikiText) return result;
+
+    var languageRegex = /\[\[(.+?)(?:\|(.+?))?\]\]/g;
+    var match;
+
+    while ((match = languageRegex.exec(wikiText))) {
+      var languageLink = match[1];
+      if (knownLanguages[languageLink]) continue;
+      knownLanguages[languageLink] = 1;
+
+      result.push({
+        name: match[2] || languageLink,
+        link: languageLink
+      });
+    }
+
+    return result;
+  }
+
+  function saveInfobox(infoBox) {
+    foundLanguages[infoBox.title].info = infoBox.info;
+  }
+
+  function save(language) {
+    foundLanguages[language.title] = language;
+  }
+
+  function addCrossLinks(languages) {
+    Object.keys(languages).forEach(addLinks);
+    return foundLanguages;
+
+    function addLinks(key) {
+      var info = languages[key].info;
+      info.parsedInfluenced.forEach(checkLinkExist);
+      info.parsedInfluencedBy.forEach(checkLinkExist);
+    }
+
+    function checkLinkExist(language) {
+      language.ref = foundLanguages[language.link];
+    }
+  }
+}
+
+function toPageid(language) {
+  return language.pageid;
+}
+
+function sanitizeDates(infoBox) {
+  return probeYear(infoBox.year) ||
+    probeYear(infoBox.released) ||
+    probeYear(infoBox.latest_release_date) ||
+    probeYear(infoBox['latest release date']);
+}
+
+function probeYear(year) {
+  if (!year) return;
+  var numericMatch = year.match(/((?:19\d\d)|(?:2\d{3}))/);
+  if (numericMatch) {
+    return parseInt(numericMatch[1], 10);
+  }
+}
