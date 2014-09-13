f5c3f8e7826b29c325aadb81b79d0a8dac33d085
anvaka <anvaka@gmail.com>
2014-09-13 14:57:04 -0700

    Rendering basic graph

--- src/scripts/appController.js
+++ src/scripts/appController.js
@@ -1,20 +1,29 @@
 require('an').controller(AppController);
 
 function AppController($scope, $http, $q) {
-  var wiki = require('./wikipediaClient')($http, $q, log);
   log('Building wikipedia graph...');
 
-  var graphBuilder = require('./languageInfluenceGraphBuilder')(wiki, log);
   var graph = require('ngraph.graph')();
-  graphBuilder.build(graph).then(function(graph) {
-    log('Done');
-    var languages = [];
-    graph.forEachNode(function (node) {
-      if (node.data) languages.push(node.data);
-    });
-
-    $scope.languages = languages.map(toView);
+  buildGraph(graph);
+  var renderer = require('ngraph.svg')(graph, {
+    container: document.getElementById('graphContainer')
   });
+
+  renderer.run();
+
+  function buildGraph(graph) {
+    var wiki = require('./wikipediaClient')($http, $q, log);
+    var graphBuilder = require('./languageInfluenceGraphBuilder')(wiki, log);
+    graphBuilder.build(graph).then(function(graph) {
+      log('Done. Found ' + graph.getNodesCount() +  ' languages and ' + graph.getLinksCount() + ' connections.');
+      var languages = [];
+      graph.forEachNode(function(node) {
+        if (node.data) languages.push(node.data);
+      });
+
+      $scope.languages = languages.map(toView);
+    });
+  }
 
   function log(message) {
     console.log(message);
